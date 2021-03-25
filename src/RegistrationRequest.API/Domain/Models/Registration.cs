0551ee5e90adb345f5809e89a1bf254f6676e531
Radwan Suufi <rd.90@hotmail.com>
2021-03-25 04:43:43 +0000

    Add Registration.cs domain model

--- src/RegistrationRequest.API/Domain/Models/Registration.cs
+++ src/RegistrationRequest.API/Domain/Models/Registration.cs
@@ -7,7 +7,6 @@
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string EmailAddress { get; set; }
-
         public Address Address { get; set; }
     }
 }
