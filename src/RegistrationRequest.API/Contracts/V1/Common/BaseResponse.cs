8d2dc72366e7de48d7e9bf89a06d58674b126cb5
Radwan Suufi <rd.90@hotmail.com>
2021-03-25 04:51:24 +0000

    Add BaseResponse.cs class.
    This will be used by CreateRegistrationResponse to create message for errors during object creation.

---
+++ src/RegistrationRequest.API/Contracts/V1/Common/BaseResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace RegistrationRequest.API.Contracts.V1.Common
+{
+    public abstract class BaseResponse
+    {
+        protected BaseResponse(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public bool Success { get; protected set; }
+        public string Message { get; protected set; }
+    }
+}
