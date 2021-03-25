aed355928eece51f39a1c1f10c972cf1c0229a68
Radwan Suufi <rd.90@hotmail.com>
2021-03-25 04:48:01 +0000

    Register our services.

---
+++ src/RegistrationRequest.API/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RegistrationRequest.API.Domain.Repositories;
+using RegistrationRequest.API.Infrastructure.Persistence;
+using RegistrationRequest.API.Infrastructure.Persistence.Repositories;
+using RegistrationRequest.API.Infrastructure.Services;
+
+namespace RegistrationRequest.API.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration["ConnectionStrings:RegistrationFormConnection"]);
+                options.EnableSensitiveDataLogging();
+            });
+
+            /// NOTE: Here we also use a scoped lifetime because these classes internally have to use the database context class.
+            /// It makes sense to specify the same scope in this case.
+            services.AddScoped<IRegistrationRepository, RegistrationRepository>();
+            services.AddScoped<IRegistrationService, RegistrationService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
--- src/RegistrationRequest.API/Startup.cs
+++ src/RegistrationRequest.API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using RegistrationRequest.API.Infrastructure;
 
 namespace RegistrationRequest.API
 {
@@ -21,6 +22,8 @@ namespace RegistrationRequest.API
         {
 
             services.AddControllers();
+            services.AddInfrastructure(Configuration);
+            services.AddAutoMapper(typeof(Startup));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Registration.API", Version = "v1" });
