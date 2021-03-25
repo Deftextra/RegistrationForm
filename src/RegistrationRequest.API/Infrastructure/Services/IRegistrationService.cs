02e6d94c3fa6f124814aee60e0cc995e0f7e83bb
Radwan Suufi <rd.90@hotmail.com>
2021-03-25 04:32:02 +0000

    Add RegistrationService.
    We want to separate the logic from our controller (or webUI in jason taylor clean architecture). Single responsibility.

--- src/RegistrationRequest.API/Infrastructure/Services/IRegistrationService.cs
+++ src/RegistrationRequest.API/Infrastructure/Services/IRegistrationService.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using RegistrationRequest.API.Contracts.V1.Responses;
 using RegistrationRequest.API.Domain.Models;
 
-namespace RegistrationRequest.API.Services
+namespace RegistrationRequest.API.Infrastructure.Services
 {
     public interface IRegistrationService
     {
         Task<IEnumerable<Registration>> GetAllRegistrationsAsync();
-        // Task<Registration> CreateRegistrationAsync(Registration registration);
+        Task<CreateRegistrationResponse> CreateRegistrationAsync(Registration registration);
         // Task<Registration> GetRegistrationByIdAsync(int id);
         // Task<Registration> UpDateRegistration();
     }
---
+++ src/RegistrationRequest.API/Infrastructure/Services/RegistrationService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using RegistrationRequest.API.Contracts.V1.Responses;
+using RegistrationRequest.API.Domain.Models;
+using RegistrationRequest.API.Domain.Repositories;
+
+namespace RegistrationRequest.API.Infrastructure.Services
+{
+    public class RegistrationService: IRegistrationService
+    {
+        private readonly IRegistrationRepository _registrationRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RegistrationService(IRegistrationRepository registrationRepository, IUnitOfWork unitOfWork)
+        {
+            _registrationRepository = registrationRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Registration>> GetAllRegistrationsAsync()
+        {
+            return await  _registrationRepository.GetAllRegistrationsAsync();
+        }
+
+        public async Task<CreateRegistrationResponse> CreateRegistrationAsync(Registration registration)
+        {
+            try
+            {
+                await _registrationRepository.CreateRegistrationAsync(registration);
+                await _unitOfWork.CompleteAsync();
+
+                return new CreateRegistrationResponse(registration);
+
+            }
+            catch (Exception ex)
+            {
+                return new CreateRegistrationResponse(
+                    $"An error occured during creation of the registration: {ex.Message}");
+            }
+        }
+
+    }
+}
