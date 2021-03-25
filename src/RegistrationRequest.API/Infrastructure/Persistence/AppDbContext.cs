52d908e14377f452bb312d3143e73eed125b3a92
Radwan Suufi <rd.90@hotmail.com>
2021-03-25 04:45:55 +0000

    Create data base.
    We use code first approach to create our database using EF core as our ORM and Sql server as our database.

---
+++ src/RegistrationRequest.API/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using RegistrationRequest.API.Domain.Repositories.Models;
+
+namespace RegistrationRequest.API.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<RegistrationDto> Registrations { get; set; }
+        public DbSet<AddresssDto> Addressses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/RegistrationRequest.API/Infrastructure/Persistence/Configurations/RegistrationDtoConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RegistrationRequest.API.Domain.Repositories.Models;
+
+namespace RegistrationRequest.API.Infrastructure.Persistence.Configurations
+{
+    public class RegistrationDtoConfigurations : IEntityTypeConfiguration<RegistrationDto>
+    {
+        public void Configure(EntityTypeBuilder<RegistrationDto> builder)
+        {
+            builder.HasIndex(r => r.Id)
+                .IsUnique();
+            builder
+                .HasKey(r => r.Id);
+            builder
+                .Property(r => r.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
